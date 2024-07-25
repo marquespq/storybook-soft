5904773f660788f8bebbac680774b5a39b08e07d
Gabriel <gabriel.marques@softdesign.com.br>
2024-07-25 13:19:15 -0300

    storybook static

---
+++ storybook-static/assets/Button.stories-Dw9OclEG.js
@@ -0,0 +1,31 @@
+import{B as P,f as x}from"./index-CRz5wbnc.js";import"./index-DZLKizrv.js";import"./index-DJO9vBfz.js";const C={title:"Example/Button",component:P,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{variant:{control:"select",options:["primary","secondary","success","danger"]}},args:{onClick:x()}},a={args:{variant:"primary",label:"Primary Button"}},r={args:{variant:"secondary",label:"Secondary Button"}},e={args:{variant:"success",label:"Success Button"}},s={args:{variant:"danger",label:"Danger Button"}},n={args:{size:"large",label:"Large Button"}},o={args:{size:"small",label:"Small Button"}};var t,c,l;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    variant: 'primary',
+    label: 'Primary Button'
+  }
+}`,...(l=(c=a.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var m,u,i;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    variant: 'secondary',
+    label: 'Secondary Button'
+  }
+}`,...(i=(u=r.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var p,g,d;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: 'success',
+    label: 'Success Button'
+  }
+}`,...(d=(g=e.parameters)==null?void 0:g.docs)==null?void 0:d.source}}};var S,y,B;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    variant: 'danger',
+    label: 'Danger Button'
+  }
+}`,...(B=(y=s.parameters)==null?void 0:y.docs)==null?void 0:B.source}}};var b,v,f;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Large Button'
+  }
+}`,...(f=(v=n.parameters)==null?void 0:v.docs)==null?void 0:f.source}}};var z,D,L;o.parameters={...o.parameters,docs:{...(z=o.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Small Button'
+  }
+}`,...(L=(D=o.parameters)==null?void 0:D.docs)==null?void 0:L.source}}};const O=["Primary","Secondary","Success","Danger","Large","Small"];export{s as Danger,n as Large,a as Primary,r as Secondary,o as Small,e as Success,O as __namedExportsOrder,C as default};
