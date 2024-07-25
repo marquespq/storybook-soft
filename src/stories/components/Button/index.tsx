0b3d4f648fe917905132db52d2c85a0e4a494b4a
Gabriel <gabriel.marques@softdesign.com.br>
2024-07-25 13:27:04 -0300

    .env e configuraçoes

--- src/stories/components/Button/index.tsx
+++ src/stories/components/Button/index.tsx
@@ -35,7 +35,7 @@ export const Button = ({
   const variantClasses = {
     primary: "bg-blue-500 hover:bg-blue-700 text-white",
     secondary: "bg-gray-500 hover:bg-gray-700 text-white",
-    success: "bg-green-500 hover:bg-green-700 text-white",
+    success: "bg-green-400 hover:bg-green-700 text-white",
     danger: "bg-red-500 hover:bg-red-700 text-white",
   };
 
