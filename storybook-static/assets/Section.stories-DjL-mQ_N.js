c5cc6dab602fd6a70dc46894f427861e3c289176
Gabriel <gabriel.marques@softdesign.com.br>
2024-07-26 10:23:32 -0300

    remove storybook-static

--- storybook-static/assets/Section.stories-DjL-mQ_N.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const i=({title:n,children:c})=>t.jsxs("section",{className:"p-4",children:[t.jsx("h2",{className:"text-xl font-bold mb-2",children:n}),c]});i.__docgenInfo={description:"",methods:[],displayName:"Section",props:{title:{required:!0,tsType:{name:"string"},description:""},children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const l={title:"Components/Section",component:i,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{title:"Minha Seção"}},e={args:{title:"Titulo teste",children:t.jsx("div",{children:"custom children"})}};var s,r,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    title: "Titulo teste",
-    children: <div>custom children</div>
-  }
-}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const m=["Title"];export{e as Title,m as __namedExportsOrder,l as default};
